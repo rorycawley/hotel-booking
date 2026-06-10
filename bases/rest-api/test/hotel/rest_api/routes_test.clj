65f8073a8ed6a2104e24ec9c710e072a68fe25d7
Rory Zac Cawley <rory.cawley@gmail.com>
2026-06-10 19:10:32 +0100

    add tests

--- bases/rest-api/test/hotel/rest_api/routes_test.clj
+++ bases/rest-api/test/hotel/rest_api/routes_test.clj
@@ -8,22 +8,82 @@
 (defn- post [system uri params]
   (routes/handler system {:request-method :post :uri uri :params params}))
 
+(defn- get* [system uri]
+  (routes/handler system {:request-method :get :uri uri :params {}}))
+
+(defn- body-json [response]
+  (json/parse-string (:body response) true))
+
+(def booking-params
+  {"guest-name" "Ada" "guest-email" "ada@example.com"
+   "check-in" "2026-07-01" "check-out" "2026-07-03"})
+
 (deftest booking-via-http-is-one-task-one-command
   (let [sys (system/test-system)
-        response (post sys "/rooms/101/book"
-                       {"guest-name" "Ada" "guest-email" "ada@example.com"
-                        "check-in" "2026-07-01" "check-out" "2026-07-03"})]
+        response (post sys "/rooms/101/book" booking-params)]
     (is (= 200 (:status response)))
-    (let [rooms (routes/handler sys {:request-method :get :uri "/rooms" :params {}})]
-      (is (= ["102" "103"]
-             (:available (json/parse-string (:body rooms) true)))))))
+    (is (= ["102" "103"]
+           (:available (body-json (get* sys "/rooms")))))))
+
+(deftest cancelling-via-http-frees-the-room
+  (let [sys (system/test-system)]
+    (post sys "/rooms/101/book" booking-params)
+    (let [response (post sys "/rooms/101/cancel" {})]
+      (is (= 200 (:status response)))
+      (is (= ["101" "102" "103"]
+             (:available (body-json (get* sys "/rooms"))))))))
+
+(deftest cancelling-an-unbooked-room-via-http-is-rejected
+  (let [sys (system/test-system)
+        response (post sys "/rooms/101/cancel" {})]
+    (is (= 409 (:status response)))
+    (is (= "room-not-booked" (:error (body-json response))))))
+
+(deftest decommissioning-via-http-removes-the-room
+  (let [sys (system/test-system)
+        response (post sys "/rooms/103/decommission" {})]
+    (is (= 200 (:status response)))
+    (is (= ["101" "102"]
+           (:available (body-json (get* sys "/rooms")))))))
+
+(deftest decommissioning-a-booked-room-via-http-is-rejected
+  (let [sys (system/test-system)]
+    (post sys "/rooms/103/book" booking-params)
+    (let [response (post sys "/rooms/103/decommission" {})]
+      (is (= 409 (:status response)))
+      (is (= "room-still-booked" (:error (body-json response))))
+      (is (= ["101" "102"]
+             (:available (body-json (get* sys "/rooms"))))))))
+
+(deftest moving-a-guest-via-http-books-new-room-and-frees-old-room
+  (let [sys (system/test-system)]
+    (post sys "/rooms/102/book" booking-params)
+    (let [response (post sys "/moves/m1/start"
+                         (assoc booking-params
+                                "from-room" "102"
+                                "to-room" "103"))]
+      (is (= 200 (:status response)))
+      (is (= ["101" "102"]
+             (:available (body-json (get* sys "/rooms"))))))))
+
+(deftest moving-to-an-already-booked-room-via-http-records-process-failure
+  (let [sys (system/test-system)]
+    (post sys "/rooms/102/book" booking-params)
+    (post sys "/rooms/103/book" (assoc booking-params
+                                       "guest-name" "Bob"
+                                       "guest-email" "bob@example.com"))
+    (let [response (post sys "/moves/m1/start"
+                         (assoc booking-params
+                                "from-room" "102"
+                                "to-room" "103"))]
+      (is (= 200 (:status response)))
+      (is (= ["101"]
+             (:available (body-json (get* sys "/rooms"))))))))
 
 (deftest a-rejected-task-maps-to-409
-  (let [sys (system/test-system)
-        params {"guest-name" "Ada" "guest-email" "ada@example.com"
-                "check-in" "2026-07-01" "check-out" "2026-07-03"}]
-    (post sys "/rooms/101/book" params)
-    (is (= 409 (:status (post sys "/rooms/101/book" params))))))
+  (let [sys (system/test-system)]
+    (post sys "/rooms/101/book" booking-params)
+    (is (= 409 (:status (post sys "/rooms/101/book" booking-params))))))
 
 (deftest crud-shaped-routes-do-not-exist
   (let [sys (system/test-system)]
--- components/booking/test/hotel/booking/cancel_booking_test.clj
+++ components/booking/test/hotel/booking/cancel_booking_test.clj
@@ -4,6 +4,9 @@
             [hotel.booking.decider :as decider]
             [hotel.booking.slices.cancel-booking.decide :as cancel]))
 
+(def ada {:name "Ada" :email "ada@example.com"})
+(def bob {:name "Bob" :email "bob@example.com"})
+
 (deftest a-booked-room-can-be-cancelled
   (is (= {:events [{:event/type :booking-cancelled :room-id "101"}]}
          (decider/decide cancel/decider
@@ -20,3 +23,15 @@
                          [{:event/type :room-booked       :room-id "101"}
                           {:event/type :booking-cancelled :room-id "101"}]
                          {:room-id "101"}))))
+
+(deftest cancelling-with-a-matching-guest-is-accepted
+  (is (= {:events [{:event/type :booking-cancelled :room-id "101"}]}
+         (decider/decide cancel/decider
+                         [{:event/type :room-booked :room-id "101" :guest ada}]
+                         {:room-id "101" :guest ada}))))
+
+(deftest cancelling-with-the-wrong-guest-is-rejected
+  (is (= {:error :booking-guest-mismatch}
+         (decider/decide cancel/decider
+                         [{:event/type :room-booked :room-id "101" :guest bob}]
+                         {:room-id "101" :guest ada}))))
